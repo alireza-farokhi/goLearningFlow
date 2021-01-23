713bea121ba515b437a97e5c113f936d6e8c83e4
Alireza Farokhi <iqstudiotehran@gmail.com>
2021-01-23 15:43:37 +0330

    variable's type declaration

--- variables/variables.go
+++ variables/variables.go
@@ -4,34 +4,10 @@ import (
 	"fmt"
 )
 
-// can be used etire package (package level variables)
-var h = "use var keyword to declare a variable outside of a function"
-
-/***
-	if uncommnet above line we'll get an errorl
-***/
-// h := "we'll get error, becuase we can't use shorthand declaration outside of a function body"
+// initialuzing type of a variable
+// false for booleans, 0 for numeric types, "" for strings, and nil for pointers, functions, interfaces, slices, channels, and maps
+var w int
 
 func main() {
-	// shorthand variable (identifire) declaration and assignment
-	// should use just inside a function body
-	// inside declared variables must used somwhere, otherwise compiler shows error
-	// the scope of this variable is in just this very function
-	x := 42
-	fmt.Println(x)
-
-	x = 99
-	fmt.Println(x)
-
-	var y = "olemon403"
-	fmt.Println(y)
-
-	// outsdie declared variables can be unused
-	fmt.Println(h)
-
-	foo()
-}
-
-func foo() {
-	fmt.Println("again:", h)
+	fmt.Println(w)
 }
