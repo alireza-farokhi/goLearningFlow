19d230b6affc88663a4b052dc06b4a44d535156d
Alireza Farokhi <iqstudiotehran@gmail.com>
2021-01-23 16:21:46 +0330

    variable types: integer

--- variables/variables.go
+++ variables/variables.go
@@ -4,10 +4,17 @@ import (
 	"fmt"
 )
 
-var y = 42
+var a int = 42
+var b int
+var c = 43
 
 func main() {
-	fmt.Println(y)
-	// get type of a certain variable using printf identifier of fmt package
-	fmt.Printf("%T\n", y)
+	fmt.Println(a)
+	fmt.Printf("%T\n", a)
+
+	fmt.Println(b)
+	fmt.Printf("%T\n", b)
+
+	fmt.Println(c)
+	fmt.Printf("%T\n", c)
 }
