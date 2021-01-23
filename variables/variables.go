3cfacc48e6a0bb90f872c9fce697b5036e61e435
Alireza Farokhi <iqstudiotehran@gmail.com>
2021-01-23 15:34:37 +0330

    variable's scope

--- variables/variables.go
+++ variables/variables.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 )
 
+// can be used etire file
 var h = "use var keyword to declare a variable outside of a function"
 
 /***
@@ -15,6 +16,7 @@ func main() {
 	// shorthand variable (identifire) declaration and assignment
 	// should use just inside a function body
 	// inside declared variables must used somwhere, otherwise compiler shows error
+	// the scope of this variable is in just this very function
 	x := 42
 	fmt.Println(x)
 
@@ -26,4 +28,10 @@ func main() {
 
 	// outsdie declared variables can be unused
 	fmt.Println(h)
+
+	foo()
+}
+
+func foo() {
+	fmt.Println("again:", h)
 }
