ffd743c8e647900311b036f9a01a9a714b3fcac2
Alireza Farokhi <iqstudiotehran@gmail.com>
2021-01-23 15:30:44 +0330

    variable declaration using 'var' keyword

--- variables/variables.go
+++ variables/variables.go
@@ -4,11 +4,26 @@ import (
 	"fmt"
 )
 
+var h = "use var keyword to declare a variable outside of a function"
+
+/***
+	if uncommnet above line we'll get an errorl
+***/
+// h := "we'll get error, becuase we can't use shorthand declaration outside of a function body"
+
 func main() {
-	// shorthand variable (identifire) declarion and assignment
+	// shorthand variable (identifire) declaration and assignment
+	// should use just inside a function body
+	// inside declared variables must used somwhere, otherwise compiler shows error
 	x := 42
 	fmt.Println(x)
 
 	x = 99
 	fmt.Println(x)
+
+	var y = "olemon403"
+	fmt.Println(y)
+
+	// outsdie declared variables can be unused
+	fmt.Println(h)
 }
